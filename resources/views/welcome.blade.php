c0476a5bd1359037bf354720ee949dc4cdb4beb6
evolution <evolution@jingdor.com>
2017-03-28 17:59:49 +0800

    更新

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Evolution
+                    Digtaldance
                 </div>
             </div>
         </div>
